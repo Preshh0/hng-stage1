6c199be3d09fad3a4689cd086c8f953cca9ce5fe
Preshh0 <uniquep201@gmail.com>
2022-10-30 07:33:00 +0100

    HNG stage one

---
+++ app.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const express = require('express');
+
+const app = express();
+/*
+app.get('api/v1/info', (req, res) => {
+
+})
+is called a route handler
+*/
+const info = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/info.json`)
+)
+app.get('api/v1/info', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            info
+        }
+    })
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on ${port}...`)
+});
