11effbbfe78880538697ab6985bd5590a3c61b43
preshh0 <uniquep201@gmail.com>
2022-10-30 22:40:36 +0100

    Restructured my folder.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const express = require('express');
+const cors = require('cors')
 
 const app = express();
 /*
@@ -8,10 +9,14 @@ app.get('api/v1/info', (req, res) => {
 })
 is called a route handler
 */
+
+app.use(cors())
+
 const info = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/info.json`)
 )
-app.get('api/v1/info', (req, res) => {
+
+app.get('/api/v1/info', (req, res) => {
     res.status(200).json({
         status: 'success',
         data: {
