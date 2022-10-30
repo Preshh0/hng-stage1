8d41077c77e29055d770b84b40dc3f8cd3e49ebe
preshh0 <uniquep201@gmail.com>
2022-10-31 00:46:26 +0100

    Made few edits

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const fs = require('fs')
+// configure dotenv and port
+
+const port = process.env.PORT || 3000;
 const express = require('express');
 const cors = require('cors')
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
@@ -14,7 +19,7 @@ app.get('/api/v1/info', (req, res) => {
     res.status(200).json(info)
 })
 
-const port = 3000;
+// const port = 3000;
 app.listen(port, () => {
     console.log(`App running on ${port}...`)
 });
