759019d1b40b37a43ac82ac3a41a74ceb2a7bbd1
preshh0 <uniquep201@gmail.com>
2022-11-05 23:40:57 +0100

    Create a POST request for the stage 2 task.

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors())
+app.use(express.json())
 
 const info = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/info.json`)
@@ -17,6 +18,46 @@ const info = JSON.parse(
 
 app.get('/api/v1/info', (req, res) => {
     res.status(200).json(info)
+})
+
+app.post('/api/v1/info', (req, res) => {
+    
+    const { operation_type, x, y } = req.body
+ 
+    const operationTypes =[ "addition", "subtraction", "multiplication" ]
+
+
+    operationTypes.includes(operation_type.toString().toLowerCase())
+
+    if (isNaN(x)) {
+        return res.status(400).send("X is invalid.");
+    }
+
+    if (isNaN(y)) {
+        return res.status(400).send("Y is invalid.");
+    }
+
+    let result = 0
+
+   if (operation_type == 'addition') {
+    result = x + y;
+   }
+
+   if (operation_type == 'subtraction') {
+    result = x - y;
+   }
+
+   if (operation_type == 'multiplication') {
+    result = x * y;
+   }
+
+  let response ={
+    slackUsername: 'preshh0',
+    operation_type: operation_type.toString().toLowerCase(),
+    result
+  };
+
+  return res.status(200).send(response);
 })
 
 // const port = 3000;
